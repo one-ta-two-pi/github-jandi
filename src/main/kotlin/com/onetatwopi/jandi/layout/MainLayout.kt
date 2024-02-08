8125f735e0f63fcd3e57a4877ef5a5626674a518
ksw <ksw@codeblock.co.kr>
2024-02-08 17:56:26 +0900

    feat: Mocking 데이터를 통해 Issue 데이터 뿌리기

--- src/main/kotlin/com/onetatwopi/jandi/layout/MainLayout.kt
+++ src/main/kotlin/com/onetatwopi/jandi/layout/MainLayout.kt
@@ -12,7 +12,13 @@ import com.onetatwopi.jandi.layout.dto.PullRequestInfo
 import com.onetatwopi.jandi.layout.panel.TabbedPanel
 import com.onetatwopi.jandi.layout.panel.issue.IssuePanel
 import com.onetatwopi.jandi.layout.panel.pullRequest.PullRequestPanel
+import kotlinx.serialization.json.*
+import java.time.Instant
 import java.time.LocalDateTime
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
+
 
 private val stubbingPullRequestData = listOf(
     PullRequestInfo("Pull Request 1", "User1", "https://www.naver.com", "Open", LocalDateTime.now()),
@@ -20,11 +26,7 @@ private val stubbingPullRequestData = listOf(
     PullRequestInfo("Pull Request 3", "User3", "https://www.naver.com", "In Progress", LocalDateTime.now())
 )
 
-private val stubbingIssueInfoData = listOf(
-    IssueInfo("Issue 1", "User3", "https://www.naver.com", "Open", LocalDateTime.now()),
-    IssueInfo("Issue 2", "User2", "https://www.naver.com", "Open", LocalDateTime.now()),
-    IssueInfo("Issue 3", "User1", "https://www.naver.com", "Open", LocalDateTime.now())
-)
+const val mockIssue = "[{\"url\":\"https://api.github.com/repos/graceful-martin/hanghae99/issues/17\",\"repository_url\":\"https://api.github.com/repos/graceful-martin/hanghae99\",\"labels_url\":\"https://api.github.com/repos/graceful-martin/hanghae99/issues/17/labels{/name}\",\"comments_url\":\"https://api.github.com/repos/graceful-martin/hanghae99/issues/17/comments\",\"events_url\":\"https://api.github.com/repos/graceful-martin/hanghae99/issues/17/events\",\"html_url\":\"https://github.com/graceful-martin/hanghae99/issues/17\",\"id\":2116919822,\"node_id\":\"I_kwDOK1ejls5-LaIO\",\"number\":17,\"title\":\"Test\",\"user\":{\"login\":\"graceful-martin\",\"id\":56020202,\"node_id\":\"MDQ6VXNlcjU2MDIwMjAy\",\"avatar_url\":\"https://avatars.githubusercontent.com/u/56020202?v=4\",\"gravatar_id\":\"\",\"url\":\"https://api.github.com/users/graceful-martin\",\"html_url\":\"https://github.com/graceful-martin\",\"followers_url\":\"https://api.github.com/users/graceful-martin/followers\",\"following_url\":\"https://api.github.com/users/graceful-martin/following{/other_user}\",\"gists_url\":\"https://api.github.com/users/graceful-martin/gists{/gist_id}\",\"starred_url\":\"https://api.github.com/users/graceful-martin/starred{/owner}{/repo}\",\"subscriptions_url\":\"https://api.github.com/users/graceful-martin/subscriptions\",\"organizations_url\":\"https://api.github.com/users/graceful-martin/orgs\",\"repos_url\":\"https://api.github.com/users/graceful-martin/repos\",\"events_url\":\"https://api.github.com/users/graceful-martin/events{/privacy}\",\"received_events_url\":\"https://api.github.com/users/graceful-martin/received_events\",\"type\":\"User\",\"site_admin\":false},\"labels\":[],\"state\":\"open\",\"locked\":false,\"assignee\":null,\"assignees\":[],\"milestone\":null,\"comments\":0,\"created_at\":\"2024-02-04T07:10:47Z\",\"updated_at\":\"2024-02-04T07:10:47Z\",\"closed_at\":null,\"author_association\":\"OWNER\",\"active_lock_reason\":null,\"body\":\"test\",\"reactions\":{\"url\":\"https://api.github.com/repos/graceful-martin/hanghae99/issues/17/reactions\",\"total_count\":0,\"+1\":0,\"-1\":0,\"laugh\":0,\"hooray\":0,\"confused\":0,\"heart\":0,\"rocket\":0,\"eyes\":0},\"timeline_url\":\"https://api.github.com/repos/graceful-martin/hanghae99/issues/17/timeline\",\"performed_via_github_app\":null,\"state_reason\":null}]"
 
 class MainLayout : ToolWindowFactory, DumbAware {
 
@@ -39,12 +41,19 @@ class MainLayout : ToolWindowFactory, DumbAware {
         val tabbedPanel = TabbedPanel()
 
         val pullRequestPanel = PullRequestPanel
-
         pullRequestPanel.setPullRequestInfoList(stubbingPullRequestData)
         pullRequestPanel.render()
 
         val issuePanel = IssuePanel
-        issuePanel.setIssueInfoList(stubbingIssueInfoData)
+
+        val jsonIssues = Json.parseToJsonElement(mockIssue).jsonArray
+        val issues = mutableListOf<IssueInfo>()
+        for (i in 0 until jsonIssues.size) {
+            val jsonIssue = jsonIssues[i].jsonObject
+            issues.add(generateIssue(jsonIssue))
+        }
+
+        issuePanel.setIssueInfoList(issues)
         issuePanel.render()
 
         tabbedPanel.addTab(pullRequestPanel)
@@ -53,5 +62,29 @@ class MainLayout : ToolWindowFactory, DumbAware {
         val contentManager = toolWindow.contentManager
         val content = contentManager.factory.createContent(tabbedPanel.getPanel(), "", false)
         contentManager.addContent(content)
+    }
+
+    private fun generateIssue(jsonIssue: JsonObject) = IssueInfo(
+        title = jsonIssue["title"]?.jsonPrimitive?.contentOrNull ?: "",
+        createUserId = jsonIssue["user"]?.jsonObject?.get("login")?.jsonPrimitive?.contentOrNull ?: "",
+        url = jsonIssue["url"]?.jsonPrimitive?.contentOrNull ?: "",
+        status = jsonIssue["state"]?.jsonPrimitive?.contentOrNull ?: "",
+        openAt = parseDateTime(jsonIssue["created_at"]?.jsonPrimitive?.contentOrNull ?: ""),
+        closeAt = parseDateTime(jsonIssue["closed_at"]?.jsonPrimitive?.contentOrNull ?: ""),
+    )
+
+    private fun parseDateTime(dateString: String?): LocalDateTime {
+        return dateString?.let {
+            try {
+                if (dateString == null || dateString == "null" || dateString == "") {
+                    return LocalDateTime.MIN
+                }
+
+                val formatter = DateTimeFormatter.ISO_INSTANT
+                LocalDateTime.ofInstant(Instant.from(formatter.parse(it)), ZoneOffset.UTC)
+            } catch (e: DateTimeParseException) {
+                throw RuntimeException("날짜 변환에 실패하였습니다.")
+            }
+        } ?: LocalDateTime.MIN
     }
 }

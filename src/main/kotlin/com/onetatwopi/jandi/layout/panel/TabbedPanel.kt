16e481a78dcb68ccba5a590eb8611b7ca0129fd3
jacob <jacob@shopperhouse.co>
2024-02-13 13:52:10 +0900

    [fix] index out of bound

--- src/main/kotlin/com/onetatwopi/jandi/layout/panel/TabbedPanel.kt
+++ src/main/kotlin/com/onetatwopi/jandi/layout/panel/TabbedPanel.kt
@@ -1,35 +1,10 @@
 package com.onetatwopi.jandi.layout.panel
 
 import com.intellij.ui.components.JBTabbedPane
-import javax.swing.*
 
 class TabbedPanel {
 
     private var panel = JBTabbedPane()
-    private var addButton = JButton("+")
-    private var refreshButton = JButton("Refresh")
-
-    init {
-        panel.tabLayoutPolicy = JTabbedPane.SCROLL_TAB_LAYOUT
-
-        val tabComponentPanel = JPanel()
-        tabComponentPanel.layout = BoxLayout(tabComponentPanel, BoxLayout.LINE_AXIS)
-        tabComponentPanel.add(Box.createHorizontalGlue())
-        tabComponentPanel.add(addButton)
-        tabComponentPanel.add(refreshButton)
-
-        panel.setTabComponentAt(0, tabComponentPanel)
-
-        addButton.addActionListener {
-            // Add your logic here for the add button
-            // For example, you can add a new tab
-        }
-
-        refreshButton.addActionListener {
-            // Add your logic here for the refresh button
-            // For example, you can refresh the content of the current tab
-        }
-    }
 
 
     fun addTab(contentPanel: ContentPanel) {

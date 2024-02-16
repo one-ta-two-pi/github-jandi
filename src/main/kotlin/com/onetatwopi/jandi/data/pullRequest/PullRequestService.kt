874e8f0289ed33e63938cd3ff0e96f4b28575bc1
bae-st <creative.apme@gmail.com>
2024-02-17 02:59:12 +0900

    fix: create branch 부분 수정, url-> html_url로 변경 및 일부 예외처리

--- src/main/kotlin/com/onetatwopi/jandi/data/pullRequest/PullRequestService.kt
+++ src/main/kotlin/com/onetatwopi/jandi/data/pullRequest/PullRequestService.kt
@@ -1,14 +1,14 @@
 package com.onetatwopi.jandi.data.pullRequest
 
+import com.intellij.openapi.ui.Messages
 import com.intellij.util.net.HTTPMethod
 import com.onetatwopi.jandi.client.Category
 import com.onetatwopi.jandi.client.GitClient
+import com.onetatwopi.jandi.client.GitClient.loginId
 import com.onetatwopi.jandi.layout.dto.PullRequestInfo
 import com.onetatwopi.jandi.layout.dto.PullRequestSubmit
 import com.onetatwopi.jandi.layout.panel.TabbedPanel
 import kotlinx.serialization.json.*
-import org.apache.http.NameValuePair
-import org.apache.http.message.BasicNameValuePair
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
@@ -45,26 +45,21 @@ class PullRequestService private constructor() {
     private fun createRepositoryPullRequest(pullRequestSubmit: PullRequestSubmit): String {
         val (title, body, head, base) = pullRequestSubmit
 
-        val requestBody = mutableMapOf(
-                "title" to title,
-                "body" to body,
-                "head" to head,
-                "base" to base
+        val headBranch = loginId?.let { head.replace("origin", it) }
+        val requestBody = mutableListOf(
+                Pair("title", title as Any),
+                Pair("body", body as Any),
+                Pair("head", headBranch as Any), // "username:develop"
+                Pair("base", base as Any), // "master"
         )
-
-        val requestBodyList: List<NameValuePair> = requestBody.map { entry ->
-            BasicNameValuePair(entry.key, entry.value)
-        }
-
-        println(TabbedPanel.getSelectedRepository())
         val response = GitClient.repoRequest(
                 method = HTTPMethod.POST,
                 repo = TabbedPanel.getSelectedRepository(),
                 category = Category.PULL,
-                body = requestBodyList
+                body = requestBody
         )
 
-//        checkResponseError(response)
+        checkResponseError(response)
 
         return response
     }
@@ -102,7 +97,7 @@ class PullRequestService private constructor() {
             title = jsonPullRequest["title"]?.jsonPrimitive?.content ?: "",
             requestUserId = jsonPullRequest["user"]?.jsonObject?.get("login")?.jsonPrimitive?.content ?: "",
             status = jsonPullRequest["state"]?.jsonPrimitive?.content ?: "",
-            url = jsonPullRequest["url"]?.jsonPrimitive?.content ?: "",
+            url = jsonPullRequest["html_url"]?.jsonPrimitive?.content ?: "",
             createdAt = createdAt ?: "yyyy-MM-dd HH:mm:ss",
             updatedAt = updatedAt ?: "yyyy-MM-dd HH:mm:ss"
         )
@@ -120,6 +115,30 @@ class PullRequestService private constructor() {
         } catch (e: Exception) {
 //            throw RuntimeException("Fail to convert local time.")
             return null
+        }
+    }
+
+    private fun checkResponseError(response: String) {
+        val jsonElement = Json.parseToJsonElement(response)
+        val jsonObject = jsonElement.jsonObject
+
+        val message = jsonObject["message"]?.jsonPrimitive?.content
+        if (message == "Validation Failed") {
+            val errors = jsonObject["errors"]?.jsonArray
+            errors?.forEach { error ->
+                val errorObj = error.jsonObject
+                val errorMessage = errorObj["message"]?.jsonPrimitive?.content
+
+                if (errorMessage != null) {
+                    if (errorMessage.indexOf("already exists") > -1) {
+                        Messages.showMessageDialog("이미 존재하는 pull request 입니다.", "Error", Messages.getErrorIcon())
+                        throw RuntimeException("이미 존재하는 pull request 입니다.")
+                    } else if (errorMessage.indexOf("No commits between") > -1) {
+                        Messages.showMessageDialog("요청하신 head > base 브랜치 사이에 커밋이 없습니다.", "Error", Messages.getErrorIcon())
+                        throw RuntimeException("요청하신 head > base 브랜치 사이에 커밋이 없습니다.")
+                    }
+                }
+            }
         }
     }
 }
